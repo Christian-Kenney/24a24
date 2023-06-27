f42ab99e17cd439c38c1262eac43e448dc2780a3
Christian-Kenney <39739946+Christian-Kenney@users.noreply.github.com>
2023-06-27 02:01:20 -0500

    rebuilding main

--- pages/components/CardListMobile.js
+++ pages/components/CardListMobile.js
@@ -5,7 +5,7 @@ import MovieCard from '../components/MovieCard.js';
 export default function CardListMobile({ movies }) {
   console.log("CARDLIST:", movies);
   if (!movies) {
-    return null; // or any fallback UI or loading state if desired
+    return null; 
   }
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '16px' }}>
